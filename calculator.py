a19f09ced7c814d125958929c4ba64a6db9e4b61
MandyamDevaKumarReddy <devakumar.mandyam1@gmail.com>
2022-11-28 21:04:30 -0600

    buttons

--- calculator.py
+++ calculator.py
@@ -12,7 +12,31 @@ def initialize(master):
 
     equation.grid(row=0, column=0, columnspan=4)
 
+    create_button(equation)
 
+def create_button(equation):
+
+    b_1 = addButton(1, equation)
+    b_2 = addButton(2, equation)
+    b_3 = addButton(3, equation)
+    b_add = addButton('+', equation)
+
+    row1 = [b_1, b_2, b_3, b_add]
+
+
+
+    row_index = 1
+    for row in [row1]:
+        col_index = 0
+        for buttn in row:
+            buttn.grid(row=row_index, column=col_index, columnspan=1)
+            col_index += 1
+        row_index += 1
+
+
+def addButton(value, equation):
+
+    return Button(master, text=value, width=17, height=7,  relief=RAISED)
 
 if __name__ == '__main__':
 
